e11f56422dd407e341d901f48ac1048d0571d1ca
“BlazingDodo git config --global user.email aryanmodi.1999@gmail.com git config --global user.name “BlazingDodo <aryanmodi.1999@gmail.com>
2021-02-20 19:23:36 -0500

    Initial Commit

---
+++ flixmebro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixmebro
+//
+//  Created by Aryan Modi on 20/02/21.
+//  Copyright © 2021 aryanmodi.1999@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
