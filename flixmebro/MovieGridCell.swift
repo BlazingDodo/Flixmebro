1d54649a040b6a8bd081061414cea007fea8f0ea
Blazing Dodo <aryanmodi.1999@gmail.com>
2021-06-06 20:54:32 -0400

    Flixmebro Part 2

---
+++ flixmebro/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixmebro
+//
+//  Created by Aryan Modi on 06/06/21.
+//  Copyright © 2021 aryanmodi.1999@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
